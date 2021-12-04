e4efb246481f90a3b53bf8cb221c9bb1f2f05b15
Daniela Romano <daniela.romano@adevinta.com>
2021-12-04 19:17:28 +0100

    day4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,30 +1,37 @@
 fun main() {
+
+    fun printTable(table: List<String>) {
+        println("Table")
+        table.map {
+            println(it)
+        }
+        println()
+    }
+
     fun calculatePoints(table: List<String>): Int {
         var summona = 0
         table.map { row ->
-            if (row != "x x x x x")
-                summona += row.split(" ").filter { it != "x" }.map { it.toInt() }.reduce { acc, i -> acc + i }
+            if (!row.contains("x x x x x"))
+                summona += row.split(" ").filter { it != "x" }.filter { it.isNotBlank() }.map { it.toInt() }.reduce { acc, i -> acc + i }
         }
         return summona
-
     }
 
     fun findWinners(tables: List<List<String>>): List<List<String>> {
         val winners = mutableListOf<List<String>>()
         tables.map { table ->
             // check row
-            if (table.contains("x x x x x")) {
+            if (table.contains("x x x x x") || table.contains(" x x x x x")) {
                 winners.add(table)
             } else {
                 if (table.filter { it.contains("x") }.size == 5) {
-                    println(table)
                     val tabellina = table.map { it.split(" ").filterNot { it.isEmpty() } }
-                    println(tabellina)
-                    tabellina.map {
-                        if (it.contains("x")) {
-                            val index = it.indexOf("x")
-                            if (tabellina.filter { it[index] == "x" }.size == 5) {
-                                winners.add(table)
+                    tabellina.firstOrNull()?.let {
+                        it.mapIndexed { index, s ->
+                            if (s.contains("x")) {
+                                if (tabellina.filter { it[index] == "x" }.size == 5) {
+                                    winners.add(table)
+                                }
                             }
                         }
                     }
@@ -35,40 +42,14 @@ fun main() {
         return winners
     }
 
-    /* fun checkTables(tables: List<List<String>>): Int {
-         tables.map { table ->
-             // check row
-             if (table.contains("x x x x x")) {
-                 return calculatePoints(table)
-             } else {
-                 if (table.filter { it.contains("x") }.size == 5) {
-                     println(table)
-                     val tabellina = table.map { it.split(" ").filter { it.isNotEmpty() } }
-                     println(tabellina)
-                     tabellina.map {
-                         if (it.contains("x")) {
-                             val index = it.indexOf("x")
-                             if (tabellina.filter { it[index] == "x" }.size == 5) {
-                                 return calculatePoints(table)
-                             }
-                         }
-                     }
-                 }
-             }
-         }
-         return -1
-     }
- */
     fun part1(numbersDrawn: List<String>, tables: List<List<String>>): Int {
         var temp = tables
-        println(temp)
         numbersDrawn.map { drawn ->
             temp = temp.map {
                 it.map {
                     it.replace("\\b$drawn\\b".toRegex(), "x")
                 }
             }
-            println(temp)
             findWinners(temp).firstOrNull()?.let {
                 return calculatePoints(it) * drawn.toInt()
             }
@@ -78,7 +59,6 @@ fun main() {
 
     fun part2(numbersDrawn: List<String>, tables: List<List<String>>): Int {
         var temp = tables
-        println(temp)
         numbersDrawn.map { drawn ->
             temp = temp.map {
                 it.map {
@@ -86,12 +66,11 @@ fun main() {
                 }
             }
             val winners = findWinners(temp)
-            temp = temp.filterNot { it in winners }
-            println("drawn = $drawn, unwinner.size = ${temp.size}")
-            println(temp.size)
-            if (temp.size == 1) {
+            if (temp.size == 1 && winners.size == 1) {
+                println("drawn = $drawn")
                 return calculatePoints(temp[0]) * drawn.toInt()
             }
+            temp = temp.filterNot { it in winners }
         }
         return -1
     }
@@ -101,6 +80,8 @@ fun main() {
 
     val numbersDrawn = input[0].split(",")
     val tables = input.subList(2, input.size).map { it.replace("\\s{2,}".toRegex(), " ") }.filter { it != "" }.chunked(5)
+    assert(part1(numbersDrawn, tables) == 38594)
+    assert(part2(numbersDrawn, tables) == 21184)
     println(part1(numbersDrawn, tables))
     println(part2(numbersDrawn, tables))
 }
