b4ab250144cf872c7af7c2eaf09def6fa1456cce
Daniela Romano <daniela.romano@adevinta.com>
2021-12-04 23:10:35 +0100

    day4 clean

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,97 +1,86 @@
 fun main() {
+    data class Row(val numbers: List<Int>)
+    data class Table(val rows: List<Row>)
 
-    fun printTable(table: List<String>) {
-        println("Table")
-        table.map {
-            println(it)
-        }
-        println()
-    }
+    val SIZE_TABLE = 5
 
-    fun isTableWinnerByRow(table: List<List<Int>>): Boolean {
-        table.map { row ->
-            if (row.count { it == -1 } == 5) {
-                return true
-            }
-        }
-        return false
-    }
-
-    fun isRowWinner(row: List<Int>): Boolean {
-        if (row.count { it == -1 } == 5) {
+    fun Row.isWinner(): Boolean {
+        if (this.numbers.count { it == -1 } == SIZE_TABLE) {
             return true
         }
         return false
     }
 
-    fun calculatePoints(table: List<List<Int>>): Int {
-        var summona = 0
-        table.map { row ->
-            if (!isRowWinner(row))
-                summona += row.filterNot { it == -1 }.reduce { acc, i -> acc + i }
+    fun Table.hasWinningRow(): Boolean {
+        if (this.rows.any { it.isWinner() }) {
+            return true
         }
-        return summona
+        return false
+    }
+
+    fun Table.hasWinningColumn(): Boolean {
+        if (this.rows.count { it.numbers.contains(-1) } == SIZE_TABLE) {
+            this.rows[0].numbers.mapIndexed { index, s ->
+                if (s == -1) {
+                    if (this.rows.count { it.numbers[index] == -1 } == SIZE_TABLE) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
     }
 
 
-    fun findWinners(tables: List<List<List<Int>>>): List<List<List<Int>>> {
-        val winners = mutableListOf<List<List<Int>>>()
+    fun Row.findNumber(drawn: Int): Row {
+        return Row(this.numbers.map { if (it == drawn) -1 else it })
+    }
+
+    fun Table.findNumber(drawn: Int): Table {
+        return Table(this.rows.map { it.findNumber(drawn) })
+    }
+
+    fun Table.calculatePoints(): Int {
+        var total = 0
+        this.rows.map { row ->
+            if (!row.isWinner())
+                total += row.numbers.filterNot { it == -1 }.reduce { acc, i -> acc + i }
+        }
+        return total
+    }
+
+
+    fun findWinners(tables: List<Table>): List<Table> {
+        val winners = mutableListOf<Table>()
         tables.map { table ->
-            // check row
-            if (isTableWinnerByRow(table)) {
+            if (table.hasWinningRow() || table.hasWinningColumn()) {
                 winners.add(table)
-            } else {
-                if (table.filter { it.contains(-1) }.size == 5) {
-                    table.first().let {
-                        it.mapIndexed { index, s ->
-                            if (s == -1) {
-                                if (table.filter { it[index] == -1 }.size == 5) {
-                                    winners.add(table)
-                                    return@let
-                                }
-                            }
-                        }
-                    }
-                } else {
-                }
             }
         }
         return winners
     }
 
-    fun part1(numbersDrawn: List<Int>, tables: List<List<List<Int>>>): Int {
+    fun part1(numbersDrawn: List<Int>, tables: List<Table>): Int {
         var temp = tables
         numbersDrawn.map { drawn ->
-            temp = temp.map {
-                it.map {
-                    it.map {
-                        if (it == drawn) -1
-                        else it
-                    }
-                }
-            }
+            temp = temp.map { table -> table.findNumber(drawn) }
+
             findWinners(temp).firstOrNull()?.let {
-                return calculatePoints(it) * drawn.toInt()
+                return it.calculatePoints() * drawn
             }
         }
         return -1
     }
 
-    fun part2(numbersDrawn: List<Int>, tables: List<List<List<Int>>>): Int {
+    fun part2(numbersDrawn: List<Int>, tables: List<Table>): Int {
         var temp = tables
         numbersDrawn.map { drawn ->
-            temp = temp.map {
-                it.map {
-                    it.map {
-                        if (it == drawn) -1
-                        else it
-                    }
-                }
+            temp = temp.map { table ->
+                table.findNumber(drawn)
             }
             val winners = findWinners(temp)
             if (temp.size == 1 && winners.size == 1) {
-                println("drawn = $drawn")
-                return calculatePoints(temp[0]) * drawn
+                return temp[0].calculatePoints() * drawn
             }
             temp = temp.filterNot { it in winners }
         }
@@ -102,7 +91,8 @@ fun main() {
     val input = readInput("Day04")
 
     val numbersDrawn = input[0].split(",").map { it.toInt() }
-    val tables = input.subList(2, input.size).map { it.replace("\\s{2,}".toRegex(), " ") }.filter { it != "" }
+    val temptables = input.subList(2, input.size)
+            .filter { it.isNotBlank() }
             .chunked(5)
             .map {
                 it.map {
@@ -110,9 +100,8 @@ fun main() {
                             .filter { it.isNotBlank() }.map { it.toInt() }
                 }
             }
+    val tabb = temptables.map { table -> Table(table.map { row -> Row(row) }) }
 
-    // assert(part1(numbersDrawn, tables) == 38594)
-    //assert(part2(numbersDrawn, tables) == 21184)
-    println(part1(numbersDrawn, tables))
-    println(part2(numbersDrawn, tables))
+    println(part1(numbersDrawn, tabb)) // 38594
+    println(part2(numbersDrawn, tabb)) // 21184
 }
